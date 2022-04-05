67ccd69cb22640283883cdcacf54f913d64dcc66
Kevin7985 <1298282@mail.ru>
2022-04-05 15:50:23 +0300

    Initial JS commit

---
+++ js/jsLib.js
@@ -0,0 +1,104 @@
+class TrelloBoard {
+  constructor(key, token, board_id) {
+    this.key = key;
+    this.token = token;
+    this.boardID = board_id;
+
+    this.checkCredentals();
+  }
+
+  async checkCredentals() {
+    let resp = await fetch(`https://api.trello.com/1/boards/${this.boardID}/?key=${this.key}&token=${this.token}`);
+    if (resp.ok) {
+      let json = await resp.json();
+    } else if (resp.status === 401) {
+      throw 'INVALID CREDENTIALS';
+    } else if (resp.status === 404) {
+      throw 'METHOD NOT FOUND';
+    } else {
+      throw 'UNEXPECTED ERROR';
+    }
+  }
+
+  async getLists() {
+    let resp = await fetch(`https://api.trello.com/1/boards/${this.boardID}/lists/?key=${this.key}&token=${this.token}`);
+    if (resp.ok) {
+      let json = (await resp.json());
+      let out = [];
+      json.forEach(item => {
+        let obj = {
+          id: item.id,
+          title: item.name
+        };
+        out.push(obj);
+      });
+      return out;
+    } else {
+      throw 'SOMETHING WENT WRONG';
+    }
+  }
+
+  async getListCards(list_id, checklists = false) {
+    let resp = await fetch(`https://api.trello.com/1/lists/${list_id}/cards/?key=${this.key}&token=${this.token}`);
+    if (resp.ok) {
+      let json = await resp.json();
+      let out = [];
+      for (const item of json) {
+        let obj = {
+          id: item.id,
+          title: item.name,
+          description: item.desc,
+          due: null,
+          checklists: []
+        };
+
+        if (item.due !== null) {
+          let date = item.due.split('.')[0];
+          let date_time = date.split('T');
+          let date_parts = date_time[0].split('-');
+          let time_parts = date_time[1].split(':');
+          date = new Date(Number(date_parts[0]), Number(date_parts[1]) - 1, Number(date_parts[2]), Number(time_parts[0]), Number(time_parts[1]), Number(time_parts[2]));
+
+          obj.due = date.getTime() / 1000;
+        }
+
+        if (!checklists) {
+          obj.checklists = item.idChecklists;
+        } else {
+          for (const id of item.idChecklists) {
+            let checklist = await this.getChecklist(id);
+            obj.checklists.push(checklist);
+          }
+        }
+
+        out.push(obj);
+      }
+      return out;
+    } else {
+      throw 'SOMETHING WENT WRONG';
+    }
+  }
+
+  async getChecklist(checklist_id) {
+    let resp = await fetch(`https://api.trello.com/1/checklists/${checklist_id}/?key=${this.key}&token=${this.token}`);
+    if (resp.ok) {
+      let json = await resp.json();
+      let obj = {
+        id: json.id,
+        title: json.name,
+        items: []
+      };
+      json.checkItems.forEach(item => {
+        let item_obj = {
+          id: item.id,
+          title: item.name,
+          state: item.state
+        };
+        obj.items.push(item_obj);
+      });
+      return obj;
+    } else {
+      throw 'SOMETHING WENT WRONG';
+    }
+  }
+}
